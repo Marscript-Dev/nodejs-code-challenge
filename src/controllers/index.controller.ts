1500329270e7aabb097da993b15841cb7565aa2f
maag0708 <aamaro@ithub.com.mx>
2024-03-16 15:10:00 -0700

    testing deploy to docker

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
+import { createTResult } from '../core/DTO/TResult';
 
 export const helloWorld = async (req: Request, res: Response) => {
-  res.send({
-    message: 'Hello World',
-  });
+  res.send(createTResult<string>('Hello World!'));
 };
